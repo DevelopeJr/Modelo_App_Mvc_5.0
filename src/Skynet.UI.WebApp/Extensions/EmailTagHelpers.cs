01e79ce9eab9bc6637832dcbff81ba38e183fb41
DevelopeJr <edemir.ferreira.jr@gmail.com>
2021-09-24 11:15:55 -0300

    Atualized

---
+++ src/Skynet.UI.WebApp/Extensions/EmailTagHelpers.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Skynet.UI.WebApp.Extensions
+{
+    public class EmailTagHelpers : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + "gmail.com";
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+
+        }
+    }
+}
--- src/Skynet.UI.WebApp/wwwroot/js/site.js
+++ src/Skynet.UI.WebApp/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Alert teste - para identificar um bug
+
+$(document).ready(function () {
+    alert("teste");
+});
